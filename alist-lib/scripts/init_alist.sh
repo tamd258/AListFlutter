a376eb74ef3780ca3750a3902a3285c7a44ecbd7
tam made <tam258@users.noreply.github.com>
2026-05-24 12:33:56 +0000

    chore: use tamd258/alist with strm driver

--- alist-lib/scripts/init_alist.sh
+++ alist-lib/scripts/init_alist.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-GIT_REPO="https://github.com/ykxVK8yL5L/alist.git"
+GIT_REPO="https://github.com/tamd258/alist.git"
 TAG_NAME=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags $GIT_REPO | tail --lines=1 | cut --delimiter='/' --fields=3)
 
 echo "AList - ${TAG_NAME}"
 rm -rf ./src
 unset GIT_WORK_TREE
-git clone https://github.com/ykxVK8yL5L/alist.git ./src
+git clone https://github.com/tamd258/alist.git ./src
 rm -rf ./src/.git
 
 mv -f ./src/* ../
